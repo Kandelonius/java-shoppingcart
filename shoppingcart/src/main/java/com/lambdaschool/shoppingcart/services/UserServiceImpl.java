da840a4ec24246941d26d6dbeb0e731e0f716d67
Shane Kaestner <shane.kaestner@yahoo.com>
2020-07-21 20:45:07 -0700

    I'm getting an error that says rawPassword cannot be null

--- shoppingcart/src/main/java/com/lambdaschool/shoppingcart/services/UserServiceImpl.java
+++ shoppingcart/src/main/java/com/lambdaschool/shoppingcart/services/UserServiceImpl.java
@@ -1,20 +1,22 @@
 package com.lambdaschool.shoppingcart.services;
 
-import com.lambdaschool.shoppingcart.exceptions.ResourceFoundException;
 import com.lambdaschool.shoppingcart.exceptions.ResourceNotFoundException;
+import com.lambdaschool.shoppingcart.models.Role;
 import com.lambdaschool.shoppingcart.models.User;
+import com.lambdaschool.shoppingcart.models.UserRoles;
 import com.lambdaschool.shoppingcart.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Transactional
 @Service(value = "userService")
 public class UserServiceImpl
-        implements UserService
+    implements UserService
 {
     /**
      * Connects this service to the users repository
@@ -25,6 +27,12 @@ public class UserServiceImpl
     @Autowired
     private CartService cartService;
 
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private HelperFunctions helperFunctions;
+
     @Override
     public List<User> findAll()
     {
@@ -34,8 +42,8 @@ public class UserServiceImpl
          * iterate over the iterator set and add each element to an array list.
          */
         userrepos.findAll()
-                .iterator()
-                .forEachRemaining(list::add);
+            .iterator()
+            .forEachRemaining(list::add);
         return list;
     }
 
@@ -43,7 +51,7 @@ public class UserServiceImpl
     public User findUserById(long id)
     {
         return userrepos.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("User id " + id + " not found!"));
+            .orElseThrow(() -> new ResourceNotFoundException("User id " + id + " not found!"));
     }
 
     @Transactional
@@ -51,24 +59,114 @@ public class UserServiceImpl
     public void delete(long id)
     {
         userrepos.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("User id " + id + " not found!"));
+            .orElseThrow(() -> new ResourceNotFoundException("User id " + id + " not found!"));
         userrepos.deleteById(id);
     }
 
+    @Override
+    public User findByName(String name)
+    {
+        User uu = userrepos.findByUsername(name.toLowerCase());
+        if (uu == null)
+        {
+            throw new EntityNotFoundException("User name " + name + " not found!");
+        }
+        return uu;
+    }
+
+    //    @Transactional
+    //    @Override
+    //    public User save(User user)
+    //    {
+    //        User newUser = new User();
+    //
+    //        newUser.setUsername(user.getUsername());
+    //        newUser.setComments(user.getComments());
+    //
+    //        if (user.getCarts()
+    //                .size() > 0)
+    //        {
+    //            throw new ResourceFoundException("Carts are not added through users");
+    //        }
+    //        return userrepos.save(newUser);
+    //    }
     @Transactional
     @Override
     public User save(User user)
     {
         User newUser = new User();
 
-        newUser.setUsername(user.getUsername());
-        newUser.setComments(user.getComments());
-
-        if (user.getCarts()
-                .size() > 0)
+        if (user.getUserid() != 0)
         {
-            throw new ResourceFoundException("Carts are not added through users");
+            userrepos.findById(user.getUserid())
+                .orElseThrow(() -> new EntityNotFoundException("User id " + user.getUserid() + " not found!"));
+            newUser.setUserid(user.getUserid());
         }
+
+        newUser.setUsername(user.getUsername()
+            .toLowerCase());
+        newUser.setPasswordNoEncrypt(user.getPassword());
+
+        newUser.getRoles()
+            .clear();
+        for (UserRoles ur : user.getRoles())
+        {
+            Role addRole = roleService.findRoleById(ur.getRole()
+                .getRoleid());
+
+            newUser.getRoles()
+                .add(new UserRoles(newUser,
+                    addRole));
+        }
+
         return userrepos.save(newUser);
     }
+
+    @Transactional
+    @Override
+    public User update(
+        User user,
+        long id)
+    {
+        User currentUser = findUserById(id);
+        if (helperFunctions.isAuthorizedToMakeChange(currentUser.getUsername()))
+        {
+
+            if (user.getUsername() != null)
+            {
+                currentUser.setUsername(user.getUsername()
+                    .toLowerCase());
+            }
+
+            if (user.getPassword() != null)
+            {
+                currentUser.setPasswordNoEncrypt(user.getPassword());
+            }
+
+            if (user.getRoles()
+                .size() > 0)
+            {
+                currentUser.getRoles()
+                    .clear();
+                for (UserRoles ur : user.getRoles())
+                {
+                    Role addRole = roleService.findRoleById(ur.getRole()
+                        .getRoleid());
+
+                    currentUser.getRoles()
+                        .add(new UserRoles(currentUser, addRole));
+                }
+            }
+
+            return userrepos.save(currentUser);
+        } else
+        {
+            {
+                // note we should never get to this line but is needed for the compiler
+                // to recognize that this exception can be thrown
+                throw new EntityNotFoundException("This user is not authorized to make change");
+            }
+        }
+    }
+
 }
